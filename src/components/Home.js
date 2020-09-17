2d4882e01aae5d0c8131a8a16d343e889886a6a8
CacyGru <71440681+CacyGru@users.noreply.github.com>
2020-09-17 22:20:39 +0200

    https://github.com/CacyGru/DrumTipApp.git

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,11 @@ function Home() {
   return (
     <div className="center">
       <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+        quisquam alias cumque dolorum! Deserunt, expedita? Deleniti cumque
+        blanditiis neque aliquam.
+      </p>
     </div>
   );
 }
